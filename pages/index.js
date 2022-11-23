f9ccd0ada4889a70a20e31a00e85e4d742718ef5
Alec Dilanchian <alec.dilanchian@gmail.com>
2022-11-23 00:37:09 -0500

    fixing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,34 @@ import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = useCallback(async () => {
+    setIsGenerating(true);
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+    const { baseChoice, finalChoice } = data;
+
+    setOutput(
+      `Song Titles:${finalChoice.text}\n\nLyrics:\n${input}${baseChoice.text}`
+    );
+
+    setIsGenerating(false);
+  }, [input]);
+
   return (
     <div className="root">
       <div className="container">
@@ -13,18 +41,43 @@ const Home = () => {
             <h2>insert your subtitle here</h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className="prompt-container">
+          <textarea className="prompt-box" value={input} onChange={onChange} />
+          <div className="prompt-buttons">
+            <div className="key-stroke">
+              <p>cmd/ctrl + enter</p>
+            </div>
+            <div className="or">
+              <p>OR</p>
+            </div>
+            <a className="generate-button" onClick={generateAction}>
+              <div className="generate">
+                {/* {!isGenerating ? (
+                  <div class="lds-ring">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                ) : ( */}
+                <p>Generate</p>
+                {/* )} */}
+              </div>
+            </a>
           </div>
-        </a>
+        </div>
+        {output && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{output}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
