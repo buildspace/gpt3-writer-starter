803c5a2ce3465210bf5fe3a3806322d006be7aa1
Alec Dilanchian <alec.dilanchian@gmail.com>
2022-11-23 11:06:00 -0500

    Adding OG

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Head from 'next/head';
 import './styles.css';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Head>
+      <title>GPT-3 Writer | buildspace</title>
+      <meta property="og:title" content="GPT-3 Writer" />
+      <meta property="og:description" content="build with buildspace" />
+      <meta
+        property="og:image"
+        content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+      />
+      <Component {...pageProps} />;
+    </Head>
+  );
 };
 
 export default App;
