5b418b28ae7cb18d69c06a50f3c926e2a00cab2b
farza <farza@buildspace.so>
2022-11-24 16:14:36 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
           property="og:image"
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
         />
+        <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
         <Main />
