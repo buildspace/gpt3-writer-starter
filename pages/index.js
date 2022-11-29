e023a1168ad8ebab4c7d1e60b919f5269a1f0349
jheave <jimenezjheave123@gmail.com>
2022-11-29 17:31:33 +0800

    fix ui

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,12 @@ const Home = () => {
             <div className="container">
                 <div className="header">
                     <div className="header-title">
-                        <h1>sup, insert your headline here</h1>
+                        <h1>Classmate</h1>
                     </div>
                     <div className="header-subtitle">
-                        <h2>insert your subtitle here</h2>
+                        <h2>A friendly AI companion that help you with your essay</h2>
                     </div>
                 </div>
-                {/* Add this code here*/}
                 <div className="prompt-container">
                    <textarea
                        className="prompt-box"
