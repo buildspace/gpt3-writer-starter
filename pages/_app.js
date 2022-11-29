e89a6ce60adca8cd9150aa2eb8c659fc6e4b932f
Aribk7 <aribkhan2004@gmail.com>
2022-11-29 01:44:54 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,9 @@
 import './styles.css';
 import { useRouter } from 'next/router';
-import posthog from 'posthog-js';
-import { useEffect } from 'react';
+
 
 function App({ Component, pageProps }) {
   const router = useRouter();
-
-  useEffect(() => {
-    // Init PostHog
-    posthog.init('phc_orRjN4marLj8TAX5Ryg6e0KuiVsdNZ8J7vEWbSQVOob', { api_host: 'https://app.posthog.com' });
-
-    // Track page views
-    const handleRouteChange = () => posthog.capture('$pageview');
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
   return <Component {...pageProps} />
 }
 export default App;
