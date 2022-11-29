ba07d8af8cec9a9da9ab78ec22b79005cefb646e
Aribk7 <aribkhan2004@gmail.com>
2022-11-29 01:46:53 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: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,
 });
 
 const openai = new OpenAIApi(configuration);
