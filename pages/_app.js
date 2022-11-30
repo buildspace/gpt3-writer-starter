d6c08a2e3f427b9ed0dae3bba0ff67c1ea475682
Aribk7 <aribkhan2004@gmail.com>
2022-11-29 17:07:51 -0800

    shipping

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import './styles.css';
 import { useRouter } from 'next/router';
-
+import {Analytics} from '@vercel/analytics';
 
 function App({ Component, pageProps }) {
   const router = useRouter();
+  <Analytics  />
   return <Component {...pageProps} />
+
+
 }
 export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="GPT-3 Writer" key="title"/>
         <meta property="og:description" content="build with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="/favicon.ico"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
