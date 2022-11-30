1d76d9ded97760f7a98c535105576a4d7e3ce86f
Aribk7 <aribkhan2004@gmail.com>
2022-11-29 17:47:38 -0800

    ff

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import './styles.css';
-import { useRouter } from 'next/router';
-import {Analytics} from '@vercel/analytics';
+import { useRouter } from 'next/router';  
+import posthog from 'posthog-js';
+
 
 function App({ Component, pageProps }) {
-  const router = useRouter();
-  <Analytics  />
-  return <Component {...pageProps} />
 
+
+posthog.init('phc_orRjN4marLj8TAX5Ryg6e0KuiVsdNZ8J7vEWbSQVOob', { api_host: 'https://app.posthog.com' });
+
+  const router = useRouter();
+
+  return <Component {...pageProps} />
 
 }
 export default App;
