3238a1e1141686444d616aaeb8f54088505d56d6
Aribk7 <aribkhan2004@gmail.com>
2022-11-29 21:42:35 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Rumi Writer" key="title"/>
-        <meta property="og:description" content="" key="description"/>
+        <meta property="og:description" content="Write an AI generated poem in the style of Rumi about anything " key="description"/>
         <meta
           property="og:image"
           content="/favicon.ico"
