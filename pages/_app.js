bce3c1ca300ee271e9b76b0576c2c120d160f941
Aribk7 <aribkhan2004@gmail.com>
2022-11-29 17:51:53 -0800

    f

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import './styles.css';
 import { useRouter } from 'next/router';  
-import posthog from 'posthog-js';
+import { usePostHog } from 'next-use-posthog';
 
 
 function App({ Component, pageProps }) {
-
-
-posthog.init('phc_orRjN4marLj8TAX5Ryg6e0KuiVsdNZ8J7vEWbSQVOob', { api_host: 'https://app.posthog.com' });
+  usePostHog('phc_orRjN4marLj8TAX5Ryg6e0KuiVsdNZ8J7vEWbSQVOob', {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    },
+  })  
 
   const router = useRouter();
 
