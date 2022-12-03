e14aad55890fa0dc04601b7dc319bc7dea37bfb7
Rabiimalik <68538407+Rabiimalik@users.noreply.github.com>
2022-12-03 19:39:19 +0500

    Add files via upload

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `
+Give me advice in the style of Plato, remember to reference all of his writings to make sure that the advice is as similar to his writings as possible and use first person as if Plato himself is replying.
+
+Question: What is the best way to live a meaningful life?
+
+Plato:  
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 300,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
