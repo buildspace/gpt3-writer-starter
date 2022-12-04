3da017b081f8fea78a486440df20a9b5f177f782
Timo Debono <timo.debono@gmail.com>
2022-12-04 21:47:16 +0200

    configure openai api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +42,38 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Generate a Cover Letter</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Write a few bullets on the job you are applying for, your previous work experience and education, and soft skills. </h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" 
+                    className="prompt-box" 
+                    value={userInput}
+                    onChange={onUserChangedText}
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+        <div className="generate">
+        {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+      </div>
+    </a>
+  </div>
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+    )}
       </div>
       <div className="badge-container grow">
         <a
