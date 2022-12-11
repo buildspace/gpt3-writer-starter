7c8ae4ee42d96d2a79aad9967dc1f9bddbf9caf8
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-11 12:30:39 -0800

    adds textarea, button, and custom headlines

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from 'next/head'
+import Image from 'next/image'
+import buildspaceLogo from '../assets/buildspace-logo.png'
+import { useState } from 'react'
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('')
+  const onUserChangedText = (event) => {
+    //TEST: text input is rendering to textbox
+    //console.log(event.target.value)
+    setUserInput(event.target.value)
+  }
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Nugg's Strain Finder</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Nugg's Strain Finder</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Let Nugg, your virtual budtender, help you find your ideal strain!</h2>
+          </div>
+          <div className="user-instructions">
+            <h3>For best results, ask your question just like you would if you were talking with Nugg in person!</h3>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            className="prompt-box"
+            placeholder="Type your desired effect or symptom relief need here..." className="prompt-box" 
+            value={userInput}
+            onChange={onUserChangedText} 
+            />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
