b6d0242be44cdb953fa7fa45314493e2717c5716
Al Cattell <alcattell@gmail.com>
2022-12-18 09:01:12 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="A.I. Lesson Planner" key="title"/>
+        <meta property="og:description" content="A GiantKelp Prototype" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://www.giantkelp.com/content/images/size/w1200/2022/09/DALL-E-2022-09-12-21.44.53---triangles--circles-and-lines-.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
