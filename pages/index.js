62c9bd9bc420e1147b88ffcf9062c3a2bb86014d
Mike Spade <mikespadeny@gmail.com>
2022-12-19 08:45:28 -0500

    add local changes  to new git branch

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,75 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
-  return (
+const [userInput, setUserInput] = useState('');
+
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+};
+ 
+ return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Project Plant | Save Time & Design</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Project Plant</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Unlock the Perfect Plants for Your Space</h2>
           </div>
         </div>
+	<div className="prompt-container">
+		<textarea placeholder="Where are we planting? Enter a city here" className="prompt-box" value={userInput} onChange={onUserChangedText}/>
+		<div className="prompt-buttons">
+    			<a className={isGenerating ? 'generate-button loading' : 'generate-button'}  onClick={callGenerateEndpoint}>
+      				<div className="generate">
+        				{isGenerating ? <span className='loader'></span> : <p>Generate</p>}
+      				</div>
+    			</a>
+  		</div>
+		{apiOutput && (
+  		<div className="output">
+    			<div className="output-header-container">
+      				<div className="output-header">
+        				<h3>Output</h3>
+      				</div>
+    			</div>
+    			<div className="output-content">
+      				<p>{apiOutput}</p>
+    			</div>
+  		</div>
+)}
+	</div>
       </div>
       <div className="badge-container grow">
         <a
@@ -35,3 +88,4 @@ const Home = () => {
 };
 
 export default Home;
+
