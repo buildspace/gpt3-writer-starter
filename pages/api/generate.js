5d4dbd670dd21a4b7ca77dff5af3307acbc4a917
ashesashes <ashi.mbamail@gmail.com>
2022-12-20 20:55:47 -0800

    chaining prompts - v1
    takes an input and generates a story

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,9 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix = 
 `
-Write me a response in the style of Albus Dumbledore, headmaster at Hogwarts, AMA style. Please make sure the response is conversational and open-ended and explains concepts in the style of Dumbledore. Please make sure that there is a hook that invites people to add to the conversation. Please make sure that muggle concepts or questions related to the muggle world are explained appropriately at a 10 year old reading level. 
+Please write me a fanfiction story set in the Harry Potter Universe with an original character or characters for the title below. Please make sure that the response outputs a title, an opening paragraph in a style like JK Rowling. Make it awesome.
+
+Title: 
 `
 const generateAction = async (req, res) => {
   //logging errors
@@ -17,16 +19,53 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
+    temperature: 0.8,
     max_tokens: 250,
   });
   
-  const basePromptOutput = baseCompletion.data.choices.pop();
+  console.log(baseCompletion);
+  console.dir(baseCompletion); 
 
+  const basePromptOutput = baseCompletion.data.choices.pop();
   res.status(200).json({ output: basePromptOutput });
 } catch (error) {
   res.status(500).json({ error: error.message });
   }
+//I build Prompt #2 
+const secondPrompt = 
+`
+Continue this fanfiction story set in the Harry Potter Universe with an original character that accepts a plot point that takes it in a new direction but still keeps it cohesive. Give it a title. Make it awesome like something JK Rowling would write. 
+
+Story Prompt: ${req.body.userInput}
+
+Opening Paragraph: ${basePromptOutput.text}
+
+Plot Point: 
+`
+
+ //Logging errors
+ try{
+   // I call the OpenAI API a second time with Prompt #2 
+   console.log(`API: ${secondPrompt}\n`);
+   const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    //I set a higher temperature for this one. 
+    temperature: 0.85,
+    //I also increase max_tokens
+    max_tokens: 1250,
+   });
+  
+   //Get the output 
+   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+   //Send over prompt #2's output to our UI instead of Prompt #1s
+   res.status(200).json({ output: secondPromptOutput });
+
+ } catch (error) {
+  res.status(500).json({ error: error.message });
+ }
+
 };
 
 export default generateAction;
