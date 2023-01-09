efdd0bce8fc213590393467e46d87e36cb0a4bf8
Tonnyb <tonnyb@joingladys.com>
2023-01-09 11:11:30 +0300

    adds the dialog box

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -15,6 +21,18 @@ const Home = () => {
           </div>
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput}
+            onChange={onUserChangedText} />
+
+          <div className="prompt-buttons">
+              <a className="generate-button" onClick={null}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
           </div>
         </div>
       </div>
