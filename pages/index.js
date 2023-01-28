6d163ea10460bfdfa380f92b07ce298a2ab8c7da
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-01-27 22:52:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
-
 const Home = () => {
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>574WARD Marketing Assistant</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Build The Bend</h2>
           </div>
+        </div>
+        {Textarea}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" />
         </div>
       </div>
       <div className="badge-container grow">
