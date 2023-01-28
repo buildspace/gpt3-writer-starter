1b694ebf8860ed6dd1dad98516efa480e8f25dcb
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-01-28 18:55:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>574WARD Assistant</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Build The Bend</h2>
           </div>
         </div>
       </div>
