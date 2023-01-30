7de0ec529f9cdce5f1bdedde4d27e81c2842ebac
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-01-30 06:23:25 +0000

    Pending changes exported from your codespace

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,28 @@ const Home = () => {
   const [userInput, setUserInput] = useState('');
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
+    const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
   };
 
   return (
@@ -26,7 +48,7 @@ const Home = () => {
           />
         </div>
         <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
+          <a className="generate-button" onClick={callGenerateEndpoint}>
             <div className="generate">
               <p>Generate</p>
             </div>
