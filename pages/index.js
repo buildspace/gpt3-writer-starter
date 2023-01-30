2d1a02e09e6db280db529a1d1079a34b126d167c
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-01-29 22:23:58 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
+import React, { useState } from 'react';
+
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Business Assistant</h1>
+            <h1>574WARD Marketing Assistant</h1>
           </div>
           <div className="header-subtitle">
             <h2>Build The Bend</h2>
           </div>
-        </div
+        </div>
         <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
+          <textarea 
+            placeholder="start typing here" 
+            className="prompt-box" 
+            value={userInput} 
+            onChange={onUserChangedText} 
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
         </div>
       </div>
       <div className="badge-container grow">
