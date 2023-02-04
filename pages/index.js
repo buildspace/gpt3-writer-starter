01c9025834f886cb51f547161d493a5c0cc4da59
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-03 20:51:55 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
-
+import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
-  const [apiOutput, setApiOutput] = useState("");
-  const [isGenerating, setIsGenerating] = useState(false);
-
-  const onUserChangedText = (event) => {
+  const onUserChangedText = event => {
     setUserInput(event.target.value);
   };
 
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
-    console.log("Calling OpenAI...");
 
-    const response = await fetch("/api/generate", {
-      method: "POST",
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate.js', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ userInput }),
     });
+
     const data = await response.json();
     const { output } = data;
+    console.log("OpenAI replied...", output.text)
 
-    console.log("OpenAI replied...", output.text);
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
-  };
+  }
 
   return (
     <div className="root">
@@ -42,11 +41,11 @@ const Home = () => {
           </div>
         </div>
         <div className="prompt-container">
-          <textarea
-            placeholder="start typing here"
-            className="prompt-box"
-            value={userInput}
-            onChange={onUserChangedText}
+          <textarea 
+            placeholder="start typing here" 
+            className="prompt-box" 
+            value={userInput} 
+            onChange={onUserChangedText} 
           />
         </div>
         <div className="prompt-buttons">
@@ -56,10 +55,21 @@ const Home = () => {
             </div>
           </a>
         </div>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Home;
-
