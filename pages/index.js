7fb3c5305983673938adc2b326f6f1e52148bfdb
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-03 20:54:36 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,50 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { OpenAIApi } from 'openai';
+
+const axiosInstance = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+  }
+});
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
   const onUserChangedText = event => {
     setUserInput(event.target.value);
   };
-
+  
   const [apiOutput, setApiOutput] = useState('');
   const [isGenerating, setIsGenerating] = useState(false);
-
+  
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
-
+    
     console.log("Calling OpenAI...")
-    const response = await fetch('/api/generate.js', {
+    const response =  fetch('/api/generate.js', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ userInput }),
     });
-
-    const data = await response.json();
+  
+    const data = await  response.json();
     const { output } = data;
     console.log("OpenAI replied...", output.text)
-
+  
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
   }
+  
+  const Home = () => {
+    const [userInput, setUserInput] = useState("");
+    const onUserChangedText = event => {
+      setUserInput(event.target.value);
+    };
+  }
+const apiKey = process.env.OPENAI_API_KEY;
 
   return (
     <div className="root">
@@ -56,20 +73,19 @@ const Home = () => {
           </a>
         </div>
         {apiOutput && (
-          <div className="output">
-            <div className="output-header-container">
-              <div className="output-header">
-                <h3>Output</h3>
-              </div>
-            </div>
-            <div className="output-content">
-              <p>{apiOutput}</p>
-            </div>
-          </div>
-        )}
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
       </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+</div>
     </div>
   );
 };
-
 export default Home;
