a64c8de69e518544a90bdb882c8e2b122fbb87a3
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-04 21:22:56 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,15 +1,22 @@
+// Imports the Configuration and OpenAIApi classes from the openai library
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+apiKey: process.env.OPENAI_API_KEY,
 });
 
-const api = new OpenAIApi(configuration);
+// Creates an OpenAIApi instance using the Configuration instance
+const openai = new OpenAIApi(configuration);
+
+// Define the prefix for the API prompt
 const basePromptPrefix = "";
+
+// Define the generateAction function
 const generateAction = async (req, res) => {
-  // Run first prompt
+  // Log the API request
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
+  // Call the OpenAI API to generate a response to the prompt
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
@@ -17,9 +24,12 @@ const generateAction = async (req, res) => {
     max_tokens: 250,
   });
   
+  // Get the response from the API call
   const basePromptOutput = baseCompletion.data.choices.pop();
 
+  // Return the response to the client
   res.status(200).json({ output: basePromptOutput });
 };
 
+// Export the generateAction function
 export default generateAction;
