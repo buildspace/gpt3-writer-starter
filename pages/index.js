91987530a012aeaba40f89d5f11e493e0ced3fba
borgsid <s-i-l@hotmail.it>
2023-03-02 01:38:01 +0100

    -

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -16,6 +24,22 @@ const Home = () => {
           <div className="header-subtitle">
             <h2>Ask your assistant where you want to go and what you want to do!</h2>
           </div>
+        </div>
+      </div>
+      <div className="prompt-container">
+        <textarea
+          placeholder="Ask me something or a schedule for the day"
+          className="prompt-box"
+          value={userInput}
+          onChange={onUserChangedText}
+        />
+
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Submit</p>
+            </div>
+          </a>
         </div>
       </div>
       <div className="badge-container grow">
