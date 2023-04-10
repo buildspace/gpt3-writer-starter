d4533bd2f63e57f2dd685776005b3919821b278f
alextli <alextianhongli@gmail.com>
2023-04-09 23:24:16 -0400

    Added a explain in basketball terms for boilerplate

---
+++ pages/api/generate.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`Give me a basketball analogy for the following thing. Make it include a famous or infamous moment or person in NBA history. Explain why the analogy makes sense. Elaborate on the details of the moment. Make the premise and outcome of the analogy and the moment the same. Do not repeat the prompt in the response.
+Thing: `;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 1.0,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
