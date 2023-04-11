a573b9776fb635a23c0b9965a8f255aed215644f
OrrYggE <99019434+OrrYggE@users.noreply.github.com>
2023-04-11 16:18:21 +0000

    my gpt writer starter app

--- pages/index.js
+++ pages/index.js
@@ -4,51 +4,78 @@ import { useState } from 'react';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+
+    const response = await fetch('./api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+     /* proxy: {
+        host: 'blablabla',
+        port: 81
+      },*/
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   const onUserChangedText = (event) => {
-  console.log(event.target.value);
-  setUserInput(event.target.value);
-};
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Rock song lyrics generator</h1>
+            <h1>Song lyrics generator</h1>
           </div>
           <div className="header-subtitle">
             <h2>input a topic, i'll generate lyrics </h2>
           </div>
         </div>
-        {/* Add this code here*/}
         <div className="prompt-container">
-          <textarea
-    placeholder="start typing here"
-    className="prompt-box"
-    value={userInput}
-    onChange={onUserChangedText}
-  />
-  {/* New code I added here */}
-  <div className="prompt-buttons">
-    <a className="generate-button" onClick={null}>
-      <div className="generate">
-        <p>Generate</p>
-      </div>
-    </a>
-  </div>
-          
+          <textarea className="prompt-box" placeholder="start typing here" value={userInput} onChange={onUserChangedText} />;
+        <div className="prompt-buttons">
+          <a
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
+      <div className="more-container grow">
+        Like this tool ? Download the Google Chrome Extension 
+        <a href="https://github.com/OrrYggE/LyricsGenerator/raw/main/lyrics_generator.zip" target="_blank" rel="noreferrer"> here</a>
       </div>
     </div>
   );
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+    </main>
+  )
+}
