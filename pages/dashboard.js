4cbbdd329b7ab390e07748e073f7b8cf998df638
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2023-04-11 12:30:02 -0400

    im sorry lol

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,25 +1,50 @@
-import { useSession, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { getServerSession } from 'next-auth/next';
 import Link from 'next/link';
 import Button from '../lib/button';
 import Header from '../lib/header';
+import Category from '../lib/category';
+import CategoryGrid from '../lib/category-grid';
 import { authOptions } from './api/auth/[...nextauth]';
+import Footer from '../lib/footer';
 
 function Dashboard() {
   const { data: session } = useSession();
   return (
-    <>
-      <Header>{`${session.user.name.toLowerCase()}'s dashboard`}</Header>
-      {/* <Category> */}
-      <Link href="/prompt">click here to go to a text convo w jen</Link>
-      {/* <Description></Description> */}
-      {/* </Category> */}
-      <Link href="/bookmarks">click here to see ur fav convos w jen :))</Link>
-      <Link href="/notes">click here to go to ur notes</Link>
-      <Link href="/video-call">click here to go to a video call w jen</Link>
-      <Link href="/voice-call">click here to go to an voice call w jen</Link>
-      <button onClick={() => signOut()}>sign out</button>
-    </>
+    <div>
+     <Header>ur dashboard</Header>
+      <CategoryGrid>
+        <Category>
+          <h3>prompt</h3>
+          <p>go to a text convo w jen</p>
+          <Button><Link href="/prompt">let&apos;s go!</Link></Button>
+        </Category>
+        <Category>
+          <h3>bookmarks</h3>
+          <p>see ur fav convos w jen :)</p>
+          <Button><Link href="/bookmarks">let&apos;s go!</Link></Button>
+        </Category>
+        <Category>
+          <h3>notes</h3>
+          <p>see ur highlights & notes</p>
+          <Button><Link href="/prompt">let&apos;s go!</Link></Button>
+        </Category>
+        <Category>
+          <h3>video call</h3>
+          <p>go on a video call w jen and her friends!</p>
+          <Button><Link href="/prompt">let&apos;s go!</Link></Button>
+        </Category>
+        <Category>
+          <h3>audio call</h3>
+          <p>go on an audio call w jen and her friends!</p>
+          <Button><Link href="/prompt">let&apos;s go!</Link></Button>
+        </Category>
+      </CategoryGrid>
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </div>
   );
 }
 
