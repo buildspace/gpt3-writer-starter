03849805a6a8d3fa877cfeff3547a7a1e184e93e
Aparna Jiji <130169226+Aparna-Jiji03@users.noreply.github.com>
2023-04-21 00:08:17 +0600

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,23 @@ const Home = () => {
       </div>
     </div>
   );
-};
+
+const Home = () => {
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>sup, insert your headline here</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>insert your subtitle here</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" />
+        </div>
+      </div>
 
 export default Home;
