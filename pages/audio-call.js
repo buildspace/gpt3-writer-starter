02d960183347a7a08666837b833b1d136306a35f
Tselote Woldemichael <tselotewoldemichael@gmail.com>
2023-04-22 17:23:57 -0400

    new life line - lets see how this reacts in deployment

--- pages/audio-call.js
+++ pages/audio-call.js
@@ -1,29 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { use, useEffect, useState } from 'react';
-import SpeechRecognition from 'react-speech-recognition';
+import { useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import Button from '../lib/button/button';
 import { getConversationGeneration, createFullChatContext } from '../utils/client/prompt-helpers';
-import { useSpeechSynthesis, useSpeechRecognition } from 'react-speech-kit';
+import { useSpeechSynthesis } from 'react-speech-kit';
 import Layout from '../lib/layout/layout';
-import Link from 'next/link';
+import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
+
+const appId = '703efb7d-6930-4a9e-b71c-2282c3d7d145';
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 // todo: let user pick out the voice & the rate (peep their demo & use react-speech-kit)
 // todo: add a place where the user can input how they felt about the session (abstract textarea?)
 // todo: insert session into database
 
 function AudioCall() {
-  const { listening, listen, stop, } = useSpeechRecognition({
-    onResult: (result) => {
-      setTranscript(result);
-    },
-  });
   const [isGenerating, setIsGenerating] = useState(false);
-  const [transcript, setTranscript] = useState('');
   const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis();
   const englishVoices = voices.filter(voice => voice.lang.includes('en'));
   const [chat, setChat] = useState([]);
   const [conversationStatus, setConversationStatus] = useState('not-started');
-  if (!SpeechRecognition.browserSupportsSpeechRecognition() || !supported) {
+  const { transcript, listening, browserSupportsSpeechRecognition, isMicrophoneAvailable } = useSpeechRecognition();
+  
+  if (!browserSupportsSpeechRecognition || !supported) {
     return (
       <>
         <p>
@@ -35,43 +35,55 @@ function AudioCall() {
       </>
     );
   }
-  const startListening = () => {
-    // SpeechRecognition.startListening({ continuous: true });
-    listen({ interimResults: true, lang: 'en-US' });
-  };
+
+  if (!isMicrophoneAvailable) {
+    return (
+      <>
+        <p>
+          your browser does not have access to your microphone :(
+        </p>
+        <p>
+          please check your browser settings and try again!
+        </p>
+      </>
+    );
+  }
+
+  const listenContinuously = () => SpeechRecognition.startListening({ continuous: true });
+
   const stopListening = async () => {
-    // SpeechRecognition.stopListening();
-    stop();
-    if (transcript) {
+    SpeechRecognition.stopListening();
+    // if (transcript) {
       await getResponse();
-      // resetTranscript();
-    }
+    // }
   };
+
   const getResponse = async () => {
     setIsGenerating(true);
     const prompt = createFullChatContext(chat, transcript);
-    console.log(prompt);
     const responseText = await getConversationGeneration(prompt);
     setIsGenerating(false);
     speak({ text: responseText, voice: englishVoices[0] });
     setChat([...chat, { user: transcript, JEN: responseText }]);
-    // todo: changing the order of this could help with the flow. do note: i'd have to update the latest jen's response
   };
+
   const endCall = async () => {
     if (listening) SpeechRecognition.stopListening();
     setConversationStatus('closed');
-    // resetTranscript();
     setChat([]);
     if (speaking) cancel();
-    const sessionChat = createFullChatContext(chat, transcript);
+    // const sessionChat = createFullChatContext(chat, transcript);
     // await insertAudioSessionIntoDB({sessionChat});
   };
+  
+  console.log('transcript', transcript);
+
   return (
     <>
       { conversationStatus === 'open'
           ? (
             <>
-              <h4>audio transcript</h4>
+              {/* <h4>audio transcript</h4>
               {
                 chat.length
                  ? chat.map((el) => (
@@ -84,10 +96,28 @@ function AudioCall() {
               }
               { listening ? <p>you: { transcript }</p> : null }
               { isGenerating ? <p>hmm...interesting - let me think about it.</p> : null }
-              <div style={{ display: 'flex' }}>
-                <Button onClickAction={startListening}>start recording</Button>
-                <Button onClickAction={stopListening}>stop recording</Button>
-              </div>
+              <div style={{ display: 'flex' }}> */}
+              {/* <Button
+                onTouchStart={listenContinuously}
+                onMouseDown={listenContinuously}
+                onTouchEnd={stopListening}
+                onMouseUp={stopListening}>
+                hold to talk
+              </Button> */}
+
+              {/* </div> */}
+              <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      {/* <button
+        onTouchStart={listenContinuously}
+        onMouseDown={listenContinuously}
+        onTouchEnd={SpeechRecognition.stopListening}
+        onMouseUp={SpeechRecognition.stopListening}
+      >Hold to talk</button> */}
+      <button onClick={listenContinuously}>Start</button>
+      <button onClick={stopListening}>Stop</button>
+      <p>{transcript}</p>
+    </div>
               <Button onClickAction={endCall}>end call</Button>
             </>
           )
