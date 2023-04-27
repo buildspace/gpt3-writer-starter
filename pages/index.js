eac355a30994e4180bf7d75e59cd583b6b795b71
Dustin Keith <duddykeith5683@gmail.com>
2023-04-26 23:03:45 -0700

    added code

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -11,10 +39,27 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Create Your Own Mobile Dating App</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Discover Love on the Go with a Free Mobile Dating App for Singles</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}    
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="start typing here"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          {/* new code i added here */}
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
