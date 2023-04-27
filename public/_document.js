8217e2bd2f3bba3623df0bc9cbbb0afbdb4bf93d
Dustin Keith <duddykeith5683@gmail.com>
2023-04-26 23:14:26 -0700

    moved documents

---
+++ public/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta property="og:title" content="GPT-3 Writer" key="title"/>
+        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta
+          property="og:image"
+          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image"></meta>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
