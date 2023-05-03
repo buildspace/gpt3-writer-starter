f3c2dc72f4403eb20a88c43e2f886383990c7976
Dustin Keith <duddykeith5683@gmail.com>
2023-05-03 00:29:37 -0700

    change

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import createLogo from '../assets/C.R.E.A.T.E-logo.png';
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
@@ -86,8 +86,8 @@ const callGenerateEndpoint = async () => {
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={C.R.E.A.T.E=Logo} alt="create logo" />
+            <p>Dream with C.R.E.A.T.E</p>
           </div>
         </a>
       </div>
