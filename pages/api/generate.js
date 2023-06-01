f376b12443d01ec517088276e90667784f2d0f27
Jogiter <jogiter.g@gmail.com>
2023-06-01 12:53:16 +0800

    feat: add functional pages

---
+++ pages/api/generate.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "给我生成一份专业网球教练的详细课程表，方便我可以基于以下内容来进行课程指导：";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 1000,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,87 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from 'next/head'
+import Image from 'next/image'
+import buildspaceLogo from '../assets/buildspace-logo.png'
+import { useState } from 'react'
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('')
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true)
+
+    console.log('Calling OpenAI...')
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    })
+
+    const data = await response.json()
+    const { output } = data
+    console.log('OpenAI replied...', output.text)
+
+    setApiOutput(`${output.text}`)
+    setIsGenerating(false)
+  }
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value)
+    setUserInput(event.target.value)
+  }
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Tennis class generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>be a professional tennis coach</h2>
           </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? 'generate-button loading' : 'generate-button'
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
+          </div>
+          {/* New code I added here */}
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
@@ -31,7 +97,7 @@ const Home = () => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
